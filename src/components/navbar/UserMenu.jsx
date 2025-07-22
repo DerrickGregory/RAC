93a6ca083a28be9a11c343a0bba4ff5265e86432
mugen <mugentechy@gmail.com>
2025-07-22 15:13:38 +0300

    update

--- src/components/navbar/UserMenu.jsx
+++ src/components/navbar/UserMenu.jsx
@@ -2,34 +2,23 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 
-function UserMenu({currentUser}) {
-  
- 
+function UserMenu({ currentUser }) {
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
-const dispatch = useDispatch()
-   const [isOpen, setIsOpen] = useState(false);
-    
-    const toggleOpen = useCallback(() => {
+  const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
 
-
-
- let navigate = useNavigate();
-
-
-
-
-
   return (
-    <>
- <div className="relative">
+    <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div 
-         onClick={() => navigate('/contact')}
+        <div
+          onClick={() => navigate('/contact')}
           className="
             hidden
             md:block
@@ -43,37 +32,48 @@ const dispatch = useDispatch()
             cursor-pointer
           "
         >
-           Customer Service
+          Customer Service
         </div>
-        <div 
-         onClick={toggleOpen}
-        className="
-          p-4
-          md:py-1
-          md:px-2
-          border-[1px] 
-          border-neutral-200 
-          flex 
-          flex-row 
-          items-center 
-          gap-3 
-          rounded-full 
-          cursor-pointer 
-          hover:shadow-md 
-          transition
+        <div
+          onClick={toggleOpen}
+          className="
+            p-4
+            md:py-1
+            md:px-2
+            border-[1px] 
+            border-neutral-200 
+            flex 
+            flex-row 
+            items-center 
+            gap-3 
+            rounded-full 
+            cursor-pointer 
+            hover:shadow-md 
+            transition
           "
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar  />
+            <Avatar src={currentUser?.avatar_url || ''} />
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm z-50">
+          <div className="flex flex-col cursor-pointer">
+            <MenuItem
+              label="Report"
+              onClick={() => {
+                setIsOpen(false);
+                navigate('/track');
+              }}
+            />
+          </div>
+        </div>
+      )}
     </div>
-  
-    
-    </>
-  )
+  );
 }
 
-export default UserMenu
+export default UserMenu;
